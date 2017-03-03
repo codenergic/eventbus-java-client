5db197c83b83de56fc0077761851d6ac4876dff4
dianw <dianw.aditya@gmail.com>
2017-03-03 23:42:31 +0700

    Add license

--- src/main/java/org/codenergic/eventbus/EventBusAdapter.java
+++ src/main/java/org/codenergic/eventbus/EventBusAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codenergic.eventbus;
 
 import java.io.IOException;
@@ -195,11 +210,11 @@ public final class EventBusAdapter implements EventBus {
 
 	private void onMessage(WebSocket ws, byte[] body) throws JsonProcessingException, IOException {
 		JsonNode json = objectMapper.readTree(body);
-		
+
 		if (json.has("replyAddress")) {
 			// currently not supported
 		}
-		
+
 		String address = json.get("address").textValue();
 		Message message = objectMapper.treeToValue(json, Message.class);
 
@@ -213,7 +228,7 @@ public final class EventBusAdapter implements EventBus {
 			replyHandlers.remove(address);
 			handler.handle(message);
 		} else {
-			
+
 		}
 	}
 }
